536b00fae50e973a73746c04681e634eda3308c4
Ellen <ellen31725@gmail.com>
2024-08-15 20:12:22 +0800

    feat: add consultation report page

--- src/app/private/containers/report/pages/ConsultationReportPage.tsx
+++ src/app/private/containers/report/pages/ConsultationReportPage.tsx
@@ -6,19 +6,32 @@ import { Granularity, TimePeriodType } from '../../../../../types/Share';
 import TimeFilters from '../components/TimeFilters';
 import { useState } from 'react';
 import dayjs from 'dayjs';
+import DifferentTreatmentsBarChart from '../components/DifferentTreatmentsConsultationBarChart';
+import AverageWaitingTimeLineChart from '../components/AverageWaitingTimeBarChart';
+import FeedbackBarChart from '../components/FeedbackBarChart';
 import AverageConsultationCountLineChart from '../components/AverageConsultationCountLineChart';
+import CanceledAndBookingLineChart from '../components/CanceledAndBookingConsultationLineChart';
+
+interface IFilters {
+  startDate: string;
+  endDate: string;
+  clinicId?: string;
+  doctorId?: string;
+  timePeriod?: TimePeriodType;
+  granularity: Granularity;
+}
 
 const ConsultationReportPage: React.FC = () => {
   const currentYear = dayjs().year().toString();
   const currentMonth = (dayjs().month() + 1).toString().padStart(2, '0');
   const currentWeek = dayjs().isoWeek().toString();
 
-  const [filters, setFilters] = useState({
+  const [filters, setFilters] = useState<IFilters>({
     startDate: dayjs().startOf('isoWeek').format('YYYY-MM-DD'),
     endDate: dayjs().format('YYYY-MM-DD'),
-    clinicId: '16458ab0-4bb6-4141-9bf0-6d7398942d9b',
-    doctorId: '008ce240-3f25-4e2a-a3e2-d62f5e20fe71',
-    timePeriod: TimePeriodType.MORNING_SESSION,
+    clinicId: undefined,
+    doctorId: undefined,
+    timePeriod: undefined,
     granularity: Granularity.DAY,
   });
 
@@ -48,6 +61,46 @@ const ConsultationReportPage: React.FC = () => {
           </Grid>
           <Grid item xs={12} md={12} lg={12}>
             <AverageConsultationCountLineChart
+              startDate={filters.startDate}
+              endDate={filters.endDate}
+              clinicId={filters.clinicId}
+              doctorId={filters.doctorId}
+              timePeriod={filters.timePeriod}
+              granularity={filters.granularity}
+            />
+          </Grid>
+          <Grid item xs={12} md={12} lg={12}>
+            <AverageWaitingTimeLineChart
+              startDate={filters.startDate}
+              endDate={filters.endDate}
+              clinicId={filters.clinicId}
+              doctorId={filters.doctorId}
+              timePeriod={filters.timePeriod}
+              granularity={filters.granularity}
+            />
+          </Grid>
+          <Grid item xs={12} md={12} lg={12}>
+            <CanceledAndBookingLineChart
+              startDate={filters.startDate}
+              endDate={filters.endDate}
+              clinicId={filters.clinicId}
+              doctorId={filters.doctorId}
+              timePeriod={filters.timePeriod}
+              granularity={filters.granularity}
+            />
+          </Grid>
+          <Grid item xs={12} md={12} lg={12}>
+            <DifferentTreatmentsBarChart
+              startDate={filters.startDate}
+              endDate={filters.endDate}
+              clinicId={filters.clinicId}
+              doctorId={filters.doctorId}
+              timePeriod={filters.timePeriod}
+              granularity={filters.granularity}
+            />
+          </Grid>
+          <Grid item xs={12} md={12} lg={12}>
+            <FeedbackBarChart
               startDate={filters.startDate}
               endDate={filters.endDate}
               clinicId={filters.clinicId}
