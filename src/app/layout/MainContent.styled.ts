d528ca57d6e3b25338b71d842ef26b2d7bfbb74f
Ellen <ellen31725@gmail.com>
2024-08-17 19:39:32 +0800

    feat: modify main content hight

--- src/app/layout/MainContent.styled.ts
+++ src/app/layout/MainContent.styled.ts
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 
 const MainContent = styled.div((props: { padding?: string }) => ({
   padding: props.padding || '16px',
-  minHeight: '75vh',
+  height: `calc(100vh - 64px)`,
 }));
 
 export default MainContent;
