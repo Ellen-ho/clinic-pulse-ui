7a2fb21ea5d24e6bff06585c7fa00fdc697b5af2
Ellen <ellen31725@gmail.com>
2024-08-30 14:04:22 +0800

    feat: add more top pages to nav bar

--- src/app/layout/ResponsiveAppBar.tsx
+++ src/app/layout/ResponsiveAppBar.tsx
@@ -3,7 +3,6 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthContext';
@@ -17,6 +16,17 @@ import MenuIcon from '@mui/icons-material/Menu';
 import { NotificationContext } from '../../context/NotificationContext';
 import { getNotificationHints } from '../../services/NotificationService';
 import useSWR from 'swr';
+import {
+  FiltersContext,
+  useFiltersContext,
+} from '../../context/FiltersContext';
+import { getDoctorProfile } from '../../services/DoctorService';
+
+interface Doctor {
+  id: string;
+  firstName: string;
+  lastName: string;
+}
 
 interface IPageItem {
   title: string;
@@ -29,66 +39,89 @@ interface IPage extends IPageItem {
   subMenu?: IPageItem[];
 }
 
-const topPages: IPage[] = [
-  {
-    title: '即時看板',
-    link: '/dashboard',
-    permission: [UserRoleType.DOCTOR, UserRoleType.ADMIN],
-  },
-  {
-    title: '看診紀錄',
-    link: '/consultation',
-    permission: [UserRoleType.DOCTOR, UserRoleType.ADMIN],
-  },
-  {
-    title: '反饋紀錄',
-    link: '',
-    permission: [UserRoleType.DOCTOR, UserRoleType.ADMIN],
-    subMenu: [
-      { title: '問券反饋', link: '/feedback' },
-      { title: 'Google評論', link: '/review' },
-    ],
-  },
-  {
-    title: '統計中心',
-    link: '',
-    permission: [UserRoleType.DOCTOR, UserRoleType.ADMIN],
-    subMenu: [
-      { title: '門診統計中心', link: '/consultation-report-center' },
-      { title: '反饋統計中心', link: '/feedback-report-center' },
-    ],
-  },
-  {
-    title: '人員管理',
-    link: '',
-    permission: [UserRoleType.ADMIN],
-    subMenu: [
-      { title: '人員清單', link: '/account-management' },
-      { title: '人員註冊', link: '/signup' },
-    ],
-  },
-  {
-    title: '帳號管理',
-    link: '/account-management',
-    permission: [UserRoleType.DOCTOR],
-  },
-];
-
 const ResponsiveAppBar: React.FC = () => {
   const { state, dispatch } = useContext(AuthContext);
   const isSignedIn = state.isSignedIn;
   const currentUserRole = state.currentUser?.role ?? UserRoleType.DOCTOR;
   const avatar = state.currentUser?.avatar;
+  const { doctors } = useFiltersContext();
   const [currentMenu, setCurrentMenu] = useState<IPage | null>(null);
   const { state: notificationState, dispatch: notificationDispatch } =
     useContext(NotificationContext);
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const [profileMenuAnchorEl, setProfileMenuAnchorEl] =
     useState<null | HTMLElement>(null);
-
+  const [doctorMenuAnchorEl, setDoctorMenuAnchorEl] =
+    useState<null | HTMLElement>(null);
   const navigate = useNavigate();
-
   const open = Boolean(anchorEl);
+  const doctorMenuOpen = Boolean(doctorMenuAnchorEl);
+
+  const topPages: IPage[] = [
+    {
+      title: '即時看板',
+      link: '/dashboard',
+      permission: [UserRoleType.DOCTOR, UserRoleType.ADMIN],
+    },
+    {
+      title: '看診紀錄',
+      link: '/consultation',
+      permission: [UserRoleType.DOCTOR, UserRoleType.ADMIN],
+    },
+    {
+      title: '反饋紀錄',
+      link: '',
+      permission: [UserRoleType.DOCTOR, UserRoleType.ADMIN],
+      subMenu: [
+        {
+          title: '問券反饋',
+          link: '/feedback',
+          permission: [UserRoleType.DOCTOR, UserRoleType.ADMIN],
+        },
+        {
+          title: 'Google評論',
+          link: '/review',
+          permission: [UserRoleType.ADMIN],
+        },
+      ],
+    },
+    {
+      title: '統計中心',
+      link: '',
+      permission: [UserRoleType.DOCTOR, UserRoleType.ADMIN],
+      subMenu: [
+        {
+          title: '門診統計中心',
+          link: '/consultation-report-center',
+          permission: [UserRoleType.DOCTOR, UserRoleType.ADMIN],
+        },
+        {
+          title: '反饋統計中心',
+          link: '/feedback-report-center',
+          permission: [UserRoleType.DOCTOR, UserRoleType.ADMIN],
+        },
+      ],
+    },
+    {
+      title: '門診表',
+      link: '/time-slot',
+      permission: [UserRoleType.DOCTOR, UserRoleType.ADMIN],
+    },
+    {
+      title: '人員管理',
+      link: '',
+      permission: [UserRoleType.ADMIN],
+      subMenu: [
+        { title: '人員清單', link: '/profile-management' },
+        { title: '人員註冊', link: '/signup' },
+      ],
+    },
+    {
+      title: '個人中心',
+      link: '/profile',
+      permission: [UserRoleType.DOCTOR],
+    },
+  ];
 
   const handleMenuClick = (
     event: React.MouseEvent<HTMLButtonElement>,
@@ -101,6 +134,25 @@ const ResponsiveAppBar: React.FC = () => {
       navigate(page.link);
       setAnchorEl(null);
       setCurrentMenu(null);
+    }
+  };
+
+  const handleDoctorMenuClick = (event: React.MouseEvent<HTMLElement>) => {
+    setDoctorMenuAnchorEl(event.currentTarget);
+  };
+
+  const handleDoctorMenuClose = () => {
+    setDoctorMenuAnchorEl(null);
+  };
+
+  const handleDoctorProfileClick = async (doctorId: string) => {
+    try {
+      const doctorProfile = await getDoctorProfile({ doctorId });
+      navigate(`/profile?doctorId=${doctorId}`);
+    } catch (error) {
+      console.error('Failed to fetch doctor profile:', error);
+    } finally {
+      handleDoctorMenuClose();
     }
   };
 
@@ -145,7 +197,6 @@ const ResponsiveAppBar: React.FC = () => {
     () => getNotificationHints(),
     {
       onSuccess: (data) => {
-        console.log(data.hasUnReadNotification);
         notificationDispatch({
           type: 'UPDATE_NOTIFICATION',
           payload: {
@@ -226,14 +277,43 @@ const ResponsiveAppBar: React.FC = () => {
                   open={open}
                   onClose={handleClose}
                 >
-                  {currentMenu?.subMenu?.map((subPage) => (
-                    <MenuItem
-                      key={subPage.title}
-                      onClick={() => handleNavigate(subPage.link)}
+                  {currentMenu?.subMenu
+                    ?.filter((subPage) =>
+                      subPage.permission?.includes(currentUserRole),
+                    )
+                    .map((subPage) => (
+                      <MenuItem
+                        key={subPage.title}
+                        onClick={() => handleNavigate(subPage.link)}
+                        onMouseEnter={
+                          subPage.title === '人員清單'
+                            ? handleDoctorMenuClick
+                            : undefined
+                        }
+                      >
+                        {subPage.title}
+                      </MenuItem>
+                    ))}
+                  {/* 醫師子列表 */}
+                  {currentMenu?.title === '人員管理' && (
+                    <Menu
+                      anchorEl={doctorMenuAnchorEl}
+                      open={Boolean(doctorMenuAnchorEl)}
+                      onClose={handleDoctorMenuClose}
+                      MenuListProps={{
+                        onMouseLeave: handleDoctorMenuClose,
+                      }}
                     >
-                      {subPage.title}
-                    </MenuItem>
-                  ))}
+                      {doctors.map((doctor) => (
+                        <MenuItem
+                          key={doctor.id}
+                          onClick={() => handleDoctorProfileClick(doctor.id)}
+                        >
+                          {doctor.fullName} {}
+                        </MenuItem>
+                      ))}
+                    </Menu>
+                  )}
                 </Menu>
                 <IconButton
                   sx={{ color: 'white' }}
@@ -248,19 +328,27 @@ const ResponsiveAppBar: React.FC = () => {
                     <NotificationsIcon />
                   </Badge>
                 </IconButton>
-                <IconButton
-                  sx={{ color: 'white' }}
-                  onClick={handleProfileMenuOpen}
-                >
-                  <ImageAvatar
-                    imageUrl={avatar}
-                    sx={{
-                      display: { xs: 'none', md: 'flex' },
-                    }}
-                  />
-                  <MenuIcon sx={{ display: { xs: 'flex', md: 'none' } }} />
-                </IconButton>
-
+                {currentUserRole !== UserRoleType.ADMIN ? (
+                  <IconButton
+                    sx={{ color: 'white' }}
+                    onClick={handleProfileMenuOpen}
+                  >
+                    <ImageAvatar
+                      imageUrl={avatar}
+                      sx={{
+                        display: { xs: 'none', md: 'flex' },
+                      }}
+                    />
+                    <MenuIcon sx={{ display: { xs: 'flex', md: 'none' } }} />
+                  </IconButton>
+                ) : (
+                  <Button
+                    onClick={handleSignOut}
+                    sx={{ color: 'white', display: { xs: 'none', md: 'flex' } }}
+                  >
+                    登出
+                  </Button>
+                )}
                 <Menu
                   anchorEl={profileMenuAnchorEl}
                   open={Boolean(profileMenuAnchorEl)}
